20ac0e98fc3ed07c960fc2517bf8ef50892f91e2
lizbethvaladez <0950014@my.scccd.edu>
2025-11-10 13:11:16 -0800

    Added name, code description, and comments for planned functions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 // CSCI-40 Final Project
-// Author: [Your Name Here]
+// Author: Lizbeth Valadez
 
 /* 
  * Description:
- *
+ * A music library program where users can add or remove songs that are read and stored on a file. 
+ * A menu will give options to add, remove, search, and sort the library. The search will be through song names
+ * and artists. There will be options to sort by name and artist. 
+ * (WIP) There will be an option to create playlists from the main music library, potentially using pointers
+ * to add the songs from the library
 */
+
+//struct for library
 
 #include <iostream>
 
 int main() {
     return 0;
 }
+
+//reading library file function into a dynamic array
+//print playlist function
+//add songs function
+//remove songs function
+//search function
+//sorting function
