cce9996f785260019ebcfe6202040f73ea1e18f8
lizbethvaladez <0950014@my.scccd.edu>
2025-11-12 02:50:20 -0800

    Added new planned functions in comments, plan for a switch break menu. Added program welcome message.

--- main.cpp
+++ main.cpp
@@ -23,12 +23,25 @@ struct musicLibrary {
 
 
 int main() {
+    //printing welcome to program
+    cout << "--------   Welcome to your Music Library  --------" << endl;
+
+    //load playlist
+
+    //menu for actions (switch loop?)
+    // - view library, edit playlist(remove or add song), search, 
+    // and sort playlist a certain way (potentially create playlist)
+
+
     return 0;
 }
 
 //reading library file function into a dynamic array
-//print playlist function
+//print library function
 //add songs function
 //remove songs function
 //search function
 //sorting function
+// Maybe:
+//create playlist function
+//delete playlist function
