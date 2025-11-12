5db4770051bd2d2fa1da8885dd1c236c02413d78
lizbethvaladez <0950014@my.scccd.edu>
2025-11-12 03:13:11 -0800

    Implemented switch statement menu with inputs to int action. Functions with tests, except for the retry statement.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,6 @@ struct musicLibrary {
 };
 
 
-
 int main() {
     //printing welcome to program
     cout << "--------   Welcome to your Music Library  --------" << endl;
@@ -29,9 +28,37 @@ int main() {
     //load playlist
 
     //menu for actions (switch loop?)
-    // - view library, edit playlist(remove or add song), search, 
+    // - view library, edit library(remove or add song), search, end program 
     // and sort playlist a certain way (potentially create playlist)
+    int action;
+    cout << "\nWhat would you like to do?(enter number)\n"
+        << "1: View Libray 2: Edit Library 3: Search 4: Sorting Options 5: Quit\n";
+   cin >> action;
+    switch (action) {
+        case 1:
+            cout << "Option 1: View Libray" << endl;
+            break;
+        case 2:
+            cout << "Option 2: Edit Library" << endl;
+            break;
+        case 3:
+            cout << "Option 3: Search" << endl;
+            break;
+        case 4:
+            cout << "Option 4: Sorting Options" << endl;
+            break;
+        case 5:
+            cout << "Option 5: Quit" << endl;
+            return 0;
+            break;
+        default:
+            cout << "Invalid Option-Retry." << endl;
+            break;
 
+
+    }
+
+    cout << "\nProgram ran." << endl;
 
     return 0;
 }
